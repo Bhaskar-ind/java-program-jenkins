8e1da3dce9e2058ba94009db3e0fa7172ab7178b
Bhaskar Jaiswal <bhaskar.jaiswal126@gmail.com>
2023-09-19 18:35:22 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[] args) {
+        int a=10,b=20,c=0;
+        c=a+b;
+        System.out.println("Value of a : " +a);
+        System.out.println("Value of b : " +b);
+        System.out.println("Addition of both number :" +c);
+    }
+}
