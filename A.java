047df39fa196b804561dc81ede7f093683cdfc2d
Bhaskar Jaiswal <bhaskar.jaiswal126@gmail.com>
2024-01-20 20:00:14 +0530

    added A.java

---
+++ A.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two number:");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=a+b;
+        System.out.println("Addition of both Number:"+c);
+        int d=a-b;
+        System.out.println("Subtraction of both Number:"+d);
+        sc.close();
+    }
+}
